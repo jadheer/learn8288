37c2eca54b95fa6051fd9a3bc659676ae7b9c72f
Jadheer Ahammed <ajadheer@gmail.com>
2019-07-07 14:25:42 +0530

    config change

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ec2-3-84-90-43.compute-1.amazonaws.com/skill/';
+// $config['base_url'] = 'http://ec2-3-84-90-43.compute-1.amazonaws.com/skill/';
+$config['base_url'] = 'http://webstaging.ml/skill/skill/';
 
 $config['env'] = 'sandbox';
 $config['api_url'] = 'https://api.sandbox.paypal.com/';
